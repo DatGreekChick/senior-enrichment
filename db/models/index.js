e8a02570abfeec7877756acd800114823d6bb9e0
Eleni A <eleni.arvanitis@me.com>
2017-10-18 11:55:31 -0400

    set up db and models working!

--- db/models/index.js
+++ db/models/index.js
@@ -1,4 +1,7 @@
 'use strict';
+const Student = require('./student'),
+      Teacher = require('./teacher'),
+      Campus = require('./campus');
 
 // Require all the models
 	// Running each model (i.e. table) module (i.e. file) registers each model into our sequelize db so any other part of the application could call db.model('user') OR db.models.user to get access to the `user` model.
@@ -6,3 +9,10 @@
 	// This is an acceptable pattern but it does have limitations in that if you change the name of the model you will have to change every time it is required everywhere
 
 // This is also probably a good place for you to set up your associations
+
+Student.belongsTo(Campus);
+
+Teacher.hasMany(Student);
+
+Campus.hasMany(Student);
+Campus.hasMany(Teacher);
