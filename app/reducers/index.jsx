ee39f53e86312b4efc5edec379b44e4e819090c1
Eleni A <eleni.arvanitis@me.com>
2017-10-18 18:05:12 -0400

    BrowserRouter and some more stuff

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -1,10 +1,15 @@
 import { combineReducers } from 'redux';
+
 import students from './students';
+import newStudentEntry from './newStudentEntry';
 import teachers from './teachers';
+import newTeacherEntry from './newTeacherEntry';
 import campuses from './campuses';
+import newCampusEntry from './newCampusEntry';
 
 // should I add a currentCampus...?
 // should I add reducers to update all categories...?
+// should I add a delete reducer for all categories...?
 const rootReducer = combineReducers({
   students,
   newStudentEntry,
