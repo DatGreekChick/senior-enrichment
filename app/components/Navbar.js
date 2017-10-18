c4b5362e7452e57c98e3bb55166017fb063e53e1
Eleni A <eleni.arvanitis@me.com>
2017-10-18 18:25:32 -0400

    some html stuff && nav-bar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -4,12 +4,30 @@ import Campuses from './Campuses';
 import Teachers from './Teachers';
 import Students from './Students';
 
+// this is hard-coded (h3's)
 const Navbar = () => {
-  return <div className="nav-bar">
-    {/*<Campuses />*/}
-    <Students />
-    {/*<Teachers />*/}
-  </div>
+  return <nav className="nav-bar">
+    <h3>Our Students</h3>
+    <h3>Our Teachers</h3>
+    <h3>Our Campuses</h3>
+  </nav>
 };
 
 export default Navbar
+
+/*
+FOR REFERENCE:
+import React, { Component } from 'react';
+import NameEntry from './NameEntry';
+
+export default class Navbar extends Component {
+  render () {
+    return (
+      <nav>
+        <h3># channelname goes here</h3>
+        <NameEntry />
+      </nav>
+    );
+  }
+}
+ */
