1eb12e382c19a4a02f6c9f6209c95fa2c184208c
Eleni A <eleni.arvanitis@me.com>
2017-10-22 11:26:08 -0400

    only rendering carousel on home page and testing emojis :raised_hands:

--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Navbar from './Navbar';
-import Carousel from './Carousel';
 import Footer from './Footer';
+
+import Carousel from './Carousel';
 import Students from './Students';
 import Teachers from './Teachers';
 import Campuses from './Campuses';
@@ -17,10 +18,10 @@ export default class Witcher extends Component {
     return (
       <div>
         <Navbar/>
-        <Carousel/>
         <Footer/>
         <main>
           <Switch>
+            <Route exact path="/" component={Carousel}/>
             <Route path="/students"
                    component={Students} />
             <Route path="/campuses"
---
+++ app/components/Teachers.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+// import store from '../store.jsx';
+
+const Teachers = () => {
+  return <div className="teachers">
+  </div>
+};
+
+export default Teachers
