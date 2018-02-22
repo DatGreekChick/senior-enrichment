10dd4d3cc4a09c58666d0f487e4e1b711e517ce2
Eleni A <eleni.arvanitis@me.com>
2018-02-22 13:57:57 -0500

    link retains css

--- app/components/Campus.js
+++ app/components/Campus.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default ({ campus }) => <li className="media">
   <div>
-    <a href={`/campuses/${campus.id}`}>
+    <Link to={`/campuses/${campus.id}`}>
       <img className="media-object"
            src={campus.image}
            alt={`${campus.name} image`}/>
-    </a>
+    </Link>
   </div>
   <div className="media-body">
     <h4 className="media-heading">
